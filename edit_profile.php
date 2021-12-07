0179e89f0eaf55ca7aae657eef335f5efa723989
[RamyaR1999] <[ramya.r@cognateglobal.com]>
2021-12-07 15:31:31 +0530

    Updated

--- edit_profile.php
+++ edit_profile.php
@@ -9,7 +9,7 @@ if(isset($_SESSION['id'])){
 
      $db_id =  $_SESSION['id'];  
            
-     $query="SELECT * FROM register WHERE id = '{$db_id}' ";
+     $query="SELECT * FROM users WHERE id = '{$db_id}' ";
      $select_register_profile = mysqli_query($connection,$query);
 
       
@@ -50,16 +50,16 @@ if(isset($_SESSION['id'])){
            $CV = $_POST['CV'];
 
 
-            $password = mysqli_real_escape_string($connection,$_POST['password']);
-            $confirm_password = mysqli_real_escape_string($connection,$_POST['confirm_password']);
-            $password = md5($password);      
-            $confirm_password = md5($confirm_password); 
+            $Password = mysqli_real_escape_string($connection,$_POST['Password']);
+            // $confirm_password = mysqli_real_escape_string($connection,$_POST['confirm_password']);
+            $Password = md5($Password);      
+            // $confirm_password = md5($confirm_password); 
           
         move_uploaded_file($image_tempname,"images/$image");
         
         if(empty($user_image)){
             
-            $query = "SELECT * FROM register WHERE id = $id ";
+            $query = "SELECT * FROM users WHERE id = $id ";
             $select_image = mysqli_query($connection,$query);
                 
             while($row = mysqli_fetch_array($select_image)){ 
@@ -69,21 +69,21 @@ if(isset($_SESSION['id'])){
             
           } 
        
-        if(preg_match('/^[\p{L} ]+$/u', $firstname)) {
+        if(preg_match('/^[\p{L} ]+$/u', $Firstname)) {
           
-        if(preg_match('/^[\p{L} ]+$/u', $lastname)) {  
+        if(preg_match('/^[\p{L} ]+$/u', $Lastname)) {  
          
-        $uppercase = preg_match('@[A-Z]@', $password);
-        $lowercase = preg_match('@[a-z]@', $password);
-        $number    = preg_match('@[0-9]@', $password);
+        $uppercase = preg_match('@[A-Z]@', $Password);
+        $lowercase = preg_match('@[a-z]@', $Password);
+        $number    = preg_match('@[0-9]@', $Password);
         $character = preg_match('/[\'^£!$%&*()}{@#~?><>,|=_+-]/', $password);
             
 
-        if(strlen($password) >= 8) { 
+        if(strlen($Password) >= 8) { 
           
-        if($password==$confirm_password){
+        // if($Password==$confirm_password){
               
-    $query="UPDATE register SET firstname= '{$firstname}', lastname= '{$lastname}', image= '{$image}', email= '{$email}',phone= '{$phone}',instagram='{$instagram}', facebook='{$facebook}',twitter='{$twitter}',youtube='{$youtube}',Location='{$Location}'  WHERE id= '{$db_id}' ";  
+    $query="UPDATE users SET Firstname= '{$Firstname}', Lastname= '{$Lastname}', Image= '{$Image}', Email= '{$Email}',Phone= '{$Phone}',City='{$City}', Industry='{$Industry}',Function='{$Function}',Education='{$Education}',Experience='{$Experience}'  WHERE id= '{$db_id}' ";  
                       
         $update_profile_query=mysqli_query($connection,$query);
 
@@ -95,11 +95,11 @@ if(isset($_SESSION['id'])){
           
            header("Location:profile.php"); 
               
-         }else{
+         // }else{
              
-           $message_confirm="password did not match";
+         //   $message_confirm="password did not match";
              
-         }
+         // }
          }else{
               $message_strnpassworad = "password contain atleast 8 characters";
               
@@ -139,14 +139,7 @@ if(isset($_SESSION['id'])){
     <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
     
     <link rel="stylesheet" href="assets/css/shared/style.css">
-
-    <style>
-img {
-  border-radius: 50%;
-}
-</style>
-
-    
+   
     <script type="application/ld+json">{
 		"@context": "http://schema.org",
 		"@type": "Organization",
@@ -336,7 +329,7 @@ img {
        <!-- <form action="" method="POST" class="u-clearfix u-form-custom-backend u-form-spacing-8 u-form-vertical u-inner-form" source="custom" name="form" style="padding: 50px;" redirect="true"> -->
       <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
         <h1 class="u-text u-text-1"><?php echo $_SESSION['Firstname'] ?></h1>
-        <p class="u-large-text u-text u-text-variant u-text-2">I'm a creative graphic designer</p>
+        <!-- <p class="u-large-text u-text u-text-variant u-text-2">I'm a creative graphic designer</p> -->
         <div class="u-clearfix u-layout-wrap u-layout-wrap-1">
           <div class="u-gutter-0 u-layout">
             <div class="u-layout-col">
@@ -349,11 +342,11 @@ img {
                    
                    <?php 
 
-                       $youtube2 = preg_replace("/https:\/\/\www.youtube.com\/watch\?v=/" , "", $youtube);
+                       // $youtube2 = preg_replace("/https:\/\/\www.youtube.com\/watch\?v=/" , "", $youtube);
                     
                     ?>
 
-<iframe width="420" height="345" src="https://www.youtube.com/embed/<?php echo $youtube2; ?>?autoplay=1&mute=0"></iframe>
+<!-- <iframe width="420" height="345" src="https://www.youtube.com/embed/<?php echo $youtube2; ?>?autoplay=1&mute=0"></iframe> -->
 
 
 <!-- <iframe style="position: absolute;top: 0;left: 0;width: 100%;height: 100%;" class="embed-responsive-item" src='https://www.youtube.com/embed/B9YKnNtFqds?playlist=B9YKnNtFqds&amp;loop=1&amp;mute=1&amp;showinfo=0&amp;controls=0&amp;start=0&amp;autoplay=1;frameborder="0" allowfullscreen="" '></iframe> -->
@@ -373,14 +366,14 @@ img {
               <br>
 
 
-            <div class="u-align-center u-container-style u-layout-cell u-palette-4-base u-size-20 u-layout-cell-2">
+            <!-- <div class="u-align-center u-container-style u-layout-cell u-palette-4-base u-size-20 u-layout-cell-2">
                 <div class="u-container-layout u-valign-middle u-container-layout-2">
                   <h3 class="u-text u-text-default u-text-3">About me</h3>
                   <p class="u-text u-text-4">I am creative graphic designer. I am&nbsp;an expert in the Adobe Creative Suit and have worked with a varied myriad of clients.&nbsp;Connecting your ideas to customer perception &amp; all the digital dots in between...</p>
                   <p class="u-text u-text-5">Image from <a href="https://www.freepik.com/photos/business" class="u-active-none u-border-1 u-border-white u-btn u-button-link u-button-style u-hover-none u-none u-text-body-alt-color u-btn-1" target="_blank">Freepik</a>
                   </p>
                 </div>
-              </div>
+              </div> -->
             </div>
               <div class="u-align-center u-container-style u-layout-cell u-size-20 u-layout-cell-3">
                 <div class="u-container-layout u-container-layout-3">
@@ -391,7 +384,7 @@ img {
                     <div class="form-group row">
                       <span style="font-weight: 600;"class="col-sm-3 col-form-label">FirstName: </span>
                          <div class="col-sm-12">
-                          <input type="text" value="<?php echo $firstname; ?>" class="form-control" name="firstname">
+                          <input type="text" value="<?php echo $Firstname; ?>" class="form-control" name="Firstname">
                           <h6 style="color:#ff0000"><?php echo $message_Firstname; ?></h6>
                            </div>
                     </div>
@@ -400,7 +393,7 @@ img {
                     <div class="form-group row">
                        <span style="font-weight: 600;" class="col-sm-3 col-form-label">LastName: </span>
                           <div class="col-sm-12">
-                             <input type="text" value="<?php echo $lastname; ?>" class="form-control" name="lastname">
+                             <input type="text" value="<?php echo $Lastname; ?>" class="form-control" name="Lastname">
                              <h6 style="color:#ff0000"><?php echo $message_Lastname; ?></h6>
                           </div>
                     </div>
@@ -411,7 +404,7 @@ img {
                     <div class="form-group row">
                       <span style="font-weight: 600;"class="col-sm-3 col-form-label">Email: </span>
                          <div class="col-sm-12">
-                          <input type="text" value="<?php echo $email; ?>" class="form-control" name="email">
+                          <input type="text" value="<?php echo $Email; ?>" class="form-control" name="Email">
                           <h6 style="color:#ff0000"><?php echo $message_email; ?></h6>
                            </div>
                     </div>
@@ -420,7 +413,7 @@ img {
                     <div class="form-group row">
                        <span style="font-weight: 600;" class="col-sm-3 col-form-label">Mobile: </span>
                           <div class="col-sm-12">
-                             <input type="text" value="<?php echo $phone; ?>" class="form-control" name="phone">
+                             <input type="text" value="<?php echo $Phone; ?>" class="form-control" name="Phone">
                              <h6 style="color:#ff0000"><?php echo $message_phone; ?></h6>
                           </div>
                     </div>
@@ -429,17 +422,17 @@ img {
               <div class="row">
                   <div class="col-md-6">
                     <div class="form-group row">
-                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Instagram: </span>
+                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Education: </span>
                          <div class="col-sm-12">
-                          <input type="text" value="<?php echo $instagram; ?>" class="form-control" name="instagram">
+                          <input type="text" value="<?php echo $Education; ?>" class="form-control" name="Education">
                         </div>
                     </div>
                  </div>
                  <div class="col-md-6">
                     <div class="form-group row">
-                       <span style="font-weight: 600;" class="col-sm-3 col-form-label">Facebook: </span>
+                       <span style="font-weight: 600;" class="col-sm-3 col-form-label">City: </span>
                           <div class="col-sm-12">
-                             <input type="text" value="<?php echo $facebook; ?>" class="form-control" name="facebook">
+                             <input type="text" value="<?php echo $City; ?>" class="form-control" name="City">
                              
                           </div>
                     </div>
@@ -448,17 +441,17 @@ img {
               <div class="row">
                   <div class="col-md-6">
                     <div class="form-group row">
-                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Twitter: </span>
+                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Industry: </span>
                          <div class="col-sm-12">
-                          <input type="text" value="<?php echo $twitter; ?>" class="form-control" name="twitter">
+                          <input type="text" value="<?php echo $Industry; ?>" class="form-control" name="Industry">
                         </div>
                     </div>
                  </div>
                  <div class="col-md-6">
                     <div class="form-group row">
-                       <span style="font-weight: 600;" class="col-sm-3 col-form-label">Youtube: </span>
+                       <span style="font-weight: 600;" class="col-sm-3 col-form-label">Function: </span>
                           <div class="col-sm-12">
-                             <input type="text" value="<?php echo $youtube; ?>" class="form-control" name="youtube">
+                             <input type="text" value="<?php echo $Function; ?>" class="form-control" name="Function">
                              
                           </div>
                     </div>
@@ -467,9 +460,9 @@ img {
               <div class="row">
                   <div class="col-md-6">
                     <div class="form-group row">
-                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Age: </span>
+                      <span style="font-weight: 600;"class="col-sm-3 col-form-label">Experience: </span>
                          <div class="col-sm-12">
-                             <input type="text" value="<?php echo $age; ?>" class="form-control" name="age">
+                             <input type="text" value="<?php echo $Experience; ?>" class="form-control" name="Experience">
                         <!-- <br>22 years<br> -->
                          
                         </div>
@@ -478,9 +471,9 @@ img {
                
                <div class="col-md-6">
                 <div class="form-group row">
-                  <span style="font-weight: 600;" class="col-sm-3 col-form-label">Location: </span>
+                  <span style="font-weight: 600;" class="col-sm-3 col-form-label">Salary: </span>
                      <div class="col-sm-12">
-                      <input id="myInput" type="text" value="<?php echo $Location; ?>" class="form-control" onFocus="geolocate()" name="Location">
+                      <input id="myInput" type="text" value="<?php echo $Salary; ?>" class="form-control" onFocus="geolocate()" name="Salary">
                           </div>
                     </div>
                  </div>
@@ -507,18 +500,42 @@ img {
       </div>
     </form>
     </section>
-    
-    <section class="u-backlink u-clearfix">
+
+ <footer class="u-clearfix u-footer" id="sec-ff43"><div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
+        <div class="u-align-left u-social-icons u-spacing-10 u-social-icons-1">
+          <a class="u-social-url" title="facebook" target="_blank" href=""><span class="u-icon u-social-facebook u-social-icon u-icon-1"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-12fb"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-12fb"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M73.5,31.6h-9.1c-1.4,0-3.6,0.8-3.6,3.9v8.5h12.6L72,58.3H60.8v40.8H43.9V58.3h-8V43.9h8v-9.2
+            c0-6.7,3.1-17,17-17h12.5v13.9H73.5z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="twitter" target="_blank" href=""><span class="u-icon u-social-icon u-social-twitter u-icon-2"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-a27c"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-a27c"><circle fill="currentColor" class="st0" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M83.8,47.3c0,0.6,0,1.2,0,1.7c0,17.7-13.5,38.2-38.2,38.2C38,87.2,31,85,25,81.2c1,0.1,2.1,0.2,3.2,0.2
+            c6.3,0,12.1-2.1,16.7-5.7c-5.9-0.1-10.8-4-12.5-9.3c0.8,0.2,1.7,0.2,2.5,0.2c1.2,0,2.4-0.2,3.5-0.5c-6.1-1.2-10.8-6.7-10.8-13.1
+            c0-0.1,0-0.1,0-0.2c1.8,1,3.9,1.6,6.1,1.7c-3.6-2.4-6-6.5-6-11.2c0-2.5,0.7-4.8,1.8-6.7c6.6,8.1,16.5,13.5,27.6,14
+            c-0.2-1-0.3-2-0.3-3.1c0-7.4,6-13.4,13.4-13.4c3.9,0,7.3,1.6,9.8,4.2c3.1-0.6,5.9-1.7,8.5-3.3c-1,3.1-3.1,5.8-5.9,7.4
+            c2.7-0.3,5.3-1,7.7-2.1C88.7,43,86.4,45.4,83.8,47.3z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="instagram" target="_blank" href=""><span class="u-icon u-social-icon u-social-instagram u-icon-3"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-7849"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-7849"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M55.9,38.2c-9.9,0-17.9,8-17.9,17.9C38,66,46,74,55.9,74c9.9,0,17.9-8,17.9-17.9C73.8,46.2,65.8,38.2,55.9,38.2
+            z M55.9,66.4c-5.7,0-10.3-4.6-10.3-10.3c-0.1-5.7,4.6-10.3,10.3-10.3c5.7,0,10.3,4.6,10.3,10.3C66.2,61.8,61.6,66.4,55.9,66.4z"></path><path fill="#FFFFFF" d="M74.3,33.5c-2.3,0-4.2,1.9-4.2,4.2s1.9,4.2,4.2,4.2s4.2-1.9,4.2-4.2S76.6,33.5,74.3,33.5z"></path><path fill="#FFFFFF" d="M73.1,21.3H38.6c-9.7,0-17.5,7.9-17.5,17.5v34.5c0,9.7,7.9,17.6,17.5,17.6h34.5c9.7,0,17.5-7.9,17.5-17.5V38.8
+            C90.6,29.1,82.7,21.3,73.1,21.3z M83,73.3c0,5.5-4.5,9.9-9.9,9.9H38.6c-5.5,0-9.9-4.5-9.9-9.9V38.8c0-5.5,4.5-9.9,9.9-9.9h34.5
+            c5.5,0,9.9,4.5,9.9,9.9V73.3z"></path></svg></span>
+          </a>
+          <a class="u-social-url" title="linkedin" target="_blank" href=""><span class="u-icon u-social-icon u-social-linkedin u-icon-4"><svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 112 112" style=""><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-fb82"></use></svg><svg class="u-svg-content" viewBox="0 0 112 112" x="0" y="0" id="svg-fb82"><circle fill="currentColor" cx="56.1" cy="56.1" r="55"></circle><path fill="#FFFFFF" d="M41.3,83.7H27.9V43.4h13.4V83.7z M34.6,37.9L34.6,37.9c-4.6,0-7.5-3.1-7.5-7c0-4,3-7,7.6-7s7.4,3,7.5,7
+            C42.2,34.8,39.2,37.9,34.6,37.9z M89.6,83.7H76.2V62.2c0-5.4-1.9-9.1-6.8-9.1c-3.7,0-5.9,2.5-6.9,4.9c-0.4,0.9-0.4,2.1-0.4,3.3v22.5
+            H48.7c0,0,0.2-36.5,0-40.3h13.4v5.7c1.8-2.7,5-6.7,12.1-6.7c8.8,0,15.4,5.8,15.4,18.1V83.7z"></path></svg></span>
+          </a>
+        </div>
+      </div></footer>
+    <section class="u-backlink u-clearfix u-footer">
       <main>
         <p>Copyright &copy; Cognate Global alphabet 2021</p>
       </main>
     </section>
 
+    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
 
-<!DOCTYPE html>
-<!-- <html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+  </body>
+</html>
+
+
+
 <style>
 * {
   box-sizing: border-box;
@@ -606,7 +623,7 @@ input[type=submit] {
 </style>
 </html>
 
-      <!-- Dropdown Profile -->
+      <!-- Dropdown Profile
       <script src="assets/vendors/js/vendor.bundle.base.js"></script>
 
   </body>
