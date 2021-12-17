6f1f526e3d66e50d1630f1fba04ad16190df306d
valarselvi <85282282+valarselvi@users.noreply.github.com>
2021-12-17 16:15:04 +0530

    update Technology-requirement

---
+++ CGBS-Technology requirement.php
@@ -0,0 +1,298 @@
+<?php session_start(); ?>
+<?php ob_start (); ?>
+<?php include "db.php"; ?>
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<!--tittle-->
+<head>
+   <meta charset="utf-8">
+   <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>CGBS-Technology requirement</title>
+   <meta name="description" content="">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- image in title-->
+   <link rel="shortcut icon" type="image/x-icon" href="image/cognate.png">
+
+   <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+      <link rel="stylesheet" href="assets/css/slicknav.css">
+      <link rel="stylesheet" href="assets/css/price_rangs.css">
+      <link rel="stylesheet" href="assets/css/animate.min.css">
+      <link rel="stylesheet" href="assets/css/magnific-popup.css">
+      <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
+      <link rel="stylesheet" href="assets/css/themify-icons.css">
+      <link rel="stylesheet" href="assets/css/slick.css">
+      <link rel="stylesheet" href="assets/css/nice-select.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <link rel="stylesheet" href="assets/css/responsive.css">
+
+
+ <link rel="stylesheet" href="nicepage.css" media="screen">
+<link rel="stylesheet" href="Page-1.css" media="screen">
+    <script class="u-script" type="text/javascript" src="jquery.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.28.7, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    
+ <!-- Profile Icon -->
+
+ <link rel="stylesheet" href="assets/css/shared/style.css">
+ <script src="assets/vendors/js/vendor.bundle.base.js"></script>
+
+ <!-- Font Awesome Icons -->
+ <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+<!-- Autocomplete -->
+<script type='text/javascript' src='js/autocomplete.js'></script>
+<link rel="stylesheet" type='text/css' href="css/autocomplete.css">
+
+   </head>
+
+   <body>
+       <!--loading time image-->
+       <div id="preloader-active">
+       <div class="preloader d-flex align-items-center justify-content-center">
+       <div class="preloader-inner position-relative">
+       <div class="preloader-circle"></div>
+       <div class="preloader-img pere-text">
+       <img src="image/cognate.png" alt="">
+</div>
+</div>
+</div>
+</div>
+
+<header>
+<div class="header-area header-transparrent">
+<div class="headder-top header-sticky">
+<div class="container">
+<div class="row align-items-center">
+<div class="col-lg-3 col-md-2">
+    <!--logoo-->
+<div class="logo">
+<a href="job_seeker.php"><img src="image/cognate.png" alt="" width="150px"> </a>
+</div>  
+</div>  
+<div class="col-lg-9 col-md-9">
+<div class="menu-wrapper">
+     <!-- Main-menu -->
+     <div class="main-menu">
+         <nav class="d-none d-lg-block">
+     <ul id="navigation">
+     <!-- <li><a href="home.php">Home</a></li> -->
+     <li><a href="Job_seeker.php">Job Seeker </a>
+     <ul class="submenu">
+         
+    <li><a href="Jobseeker_profile.php">Job Seeker Profile</a></li>
+    <li><a href="Available_jobs.php">Find A Jobs</a></li>
+    <li><a href="Register-member.php">Submit Your Resume</a></li>
+    <li><a href="career_hub.php">Career Hub</a></li>
+    <li><a href="areas_of_expertise.php">Areas of Expertise</a></li>
+    <li><a href="security_advice.php">Security Advice</a></li>
+     
+</ul>      
+</li>
+<li><a href="employer.php">employers</a>
+
+<ul class="submenu">
+         
+    <li><a href="submit_job.php">Submit A job</a></li>
+    <li><a href="request_callback.php">Request A Call Back</a></li>
+    <li><a href="areas_of_expertise.php">Areas of Expertise</a></li>
+    <li><a href="ourSolution.php">Our Solutions</a></li>
+    <li><a href="Contact.php">Contact</a></li>
+     
+</ul>
+
+</li>
+     <li><a href="About_Us.php">About</a>
+
+</li>
+     <li><a href="Contact_Us.php">Contact</a>
+</li>
+     </ul>
+       </nav>
+      </div>         
+<!--header button-->
+<div class="header-btn d-none f-right d-lg-block">
+
+   
+        <a href="Register-Member.php" class="btn head-btn1">Register</a>
+  
+        <a href="Member-Login.php" class="btn head-btn2">Login</a>
+
+
+  
+
+
+
+                              </div>
+                          </div>
+                      </div>
+                     <!--mobile button--> 
+                     <div class="col-12">
+                          <div class="mobile_menu d-block d-lg-none"></div>
+                      </div>
+                  </div>
+              </div>
+         </div>
+     </div>
+      <!-- Header End -->
+  </header>
+
+<style>
+
+  .head-btn1 {
+    margin-right: 5px;
+}
+.btn {
+    background: #fb246a;
+    -moz-user-select: none;
+    text-transform: capitalize;
+    color: #fff;
+    cursor: pointer;
+    display: inline-block;
+    font-size: 14px;
+    font-weight: 500;
+    letter-spacing: 1px;
+    line-height: 0;
+    margin-bottom: 0;
+    padding: 27px 44px;
+    border-radius: 0px;
+    margin: 10px;
+    cursor: pointer;
+    transition: color 0.4s linear;
+    position: relative;
+    z-index: 1;
+    border: 0;
+    overflow: hidden;
+    margin: 0;
+}
+
+.btn:not(:disabled):not(.disabled) {
+    cursor: pointer;
+}
+.head-btn2 {
+    background: none;
+    border: 1px solid #fb246a;
+    color: #fb246a;
+}
+
+</style>
+
+    </div>
+
+    <section class="u-clearfix u-custom-color-2 u-lightbox u-section-1" id="sec-de5a">
+       <div style="background-image: url('images/jobArea.jpg');">
+      <div class="u-clearfix u-sheet u-sheet-1">&nbsp;&nbsp;&nbsp;&nbsp;
+        <h1 class="u-align-left u-text u-text-default u-text-white u-text-2">All that you need to know </h1>
+        <h1 class="u-align-center u-text u-text-right u-text-2">About our cognate global.</h1>
+
+</div>
+  
+   </div>
+    </section>
+         <section class="u-clearfix u-grey-5 u-lightbox text-capitalize" id="sec-de5a">
+         <!-- <div class="u-align-left u-clearfix u-sheet u-sheet-1">-->
+        <p class="u-text u-text-left u-text-1"><h2><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Cognate Benefits :</strong></h2><span style="font-weight: 700;"></span>
+        </p>
+       
+          <h4>
+          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Staffing and Consulting Services.Consultative approach backed with expertise across multiple technologies.Flat Management Structure,prompt decision with <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;flexible approach to help changing dynamics of the projects.</h4>
+    </section>
+      <section>
+
+
+     <p class="u-text u-text-left u-text-1"><h2><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;which one makes us different :</strong></h2><span style="font-weight: 50;"></span>
+      <!--<a class="btn btn-primary u-align-right u-text u-text-right u-text-black u-text-1" href="/cognate global solution/why-should-you-work-with-cognate global solution/">see the difference</a>-->
+      <img src="images/download.jpg" alt="" height=640 width=1600> </p>
+
+  </section>
+   
+       <section class="u-clearfix u-grey-5 u-lightbox" id="sec-de5a">
+         <!-- <div class="u-align-right u-clearfix u-sheet u-sheet-1">-->
+        <p class="u-text u-text-center u-text-1"><h2><strong><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We are here to help ur organisation's growth :</strong></h2><span style="font-weight: 700;"></span>
+          
+      <br>
+          <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Whatever your hiring needs,We provide recruitment solutions for a wide range of technology skill.sets and requirements from custom development work to <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;engineering solutions migrations,up,grades and more.Read about our complete range of tech solutions here.<br>
+       </h4>
+     </p>
+      <!-- </div> -->
+    </section>
+    <img src="images/account-management-development.jpg" alt=""height=640 width=1600>
+    
+    <section class="u-grey-5 " id="sec-de5a">
+     
+     <div> <h1 class="u-align-left u-text u-text-left u-text-black u-text-1"></h1>
+          <!--<div class="u-align-left u-clearfix u-sheet u-sheet-1">-->
+        <p class="u-text u-text-left u-text-1"><h2><strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Provided Services :</strong></h2><span style="font-weight:;"></span></p>
+          
+    
+     <!--<h1 class="u-align-center u-text u-text-center u-text-black u-text-1">-->
+    <h4><a href="Application_service">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;1)Application services</a></h4>
+      <h4><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;2)infrastructure as service</h4></a>
+
+   <h4><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;3)end user services</a></h4>
+    <h4><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;4)embedded technologies/IOT</h4></a><!--</span>-->
+    <h4><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;5)strategy and architecture</a></h4>
+      <h4><a href="">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;6)POM services</h4></a><!--</span>-->
+
+    </div>
+  </section>
+    
+<section class="u-clearfix u-grey-5 u-lightbox" id="sec-de5a">
+          
+        <p class="u-text u-text-left u-text-1"><h1><strong><br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Insdustries we support :
+         <img src="images/industries we support (2).gif" alt="" width=1600>
+
+        </strong></h1><span style="font-weight: 700;"></span>
+          <!--<img src="images/internet.png" alt="" >-->
+        </p>
+        <p>
+        
+          <h4>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We service a wide range of customers from startups to the best-of-breed tech giants and Fortune 100 companies.Our specialist consultants and recruiters support <li>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;client requirements for talent solutions across industries from aircraft and aerospace, to pharma,retail,software development,insurance and telecommunications.</h4><br>
+          
+        </p>
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<img src="images/we support.jpg" alt=""><img src="images/we support.jpg" alt="">
+
+      </section>
+
+    <section class="u-clearfix u-custom-color-2 u-lightbox u-section-1" id="sec-de5a">
+     
+  <div style="clear: both">
+    <h1 class="text-avatar text-capitalize text-left text-monospace u-lightbox " style="inset-inline-start: initial color:red;font-size:60px;">insights and impact stories
+</h1>
+    <h3 class="text-right text-dark u-lightbox text-monospace " style="revert: right color:red;font-size:60px;"><h3></h3></h3>
+    <!--<class="u-align-center u-clearfix u-grey-5 u-section-2" id="sec-fa03">-->
+    <!--<button style=background-color:blue;> 
+    <a class="btn btn-primary" href="">talk to us</a>-->
+    
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a  class="u-border-none u-btn u-btn-round u-btn-submit u-button-style u-hover-palette-1-light-2 u-radius-3 u-pink e u-btn-1">know more
+              </a>
+              <!-- <img src="images/product management.jpg" class="" alt="" width="404" height="336"> -->
+</div>
+
+        <div class="u-expanded-width-xs u-form u-form-1">
+          
+    </section>
+    <br>
+
+       
+    <?php include "footer.php"; ?>
+
+
+        <script>
+
+const togglePassword = document.querySelector('#togglePassword');
+  const password = document.querySelector('#id_password');
+ 
+  togglePassword.addEventListener('click', function (e) {
+    // toggle the type attribute
+    const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+    password.setAttribute('type', type);
+    // toggle the eye slash icon
+    this.classList.toggle('fa-eye-slash');
+});
+
+</script>           
